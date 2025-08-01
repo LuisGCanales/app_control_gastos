e7d3c404f9cd45eb602d4b01844d5967987b92b2
luis <lgco@ciencias.unam.mx>
2025-08-01 04:06:47 -0600

    corrijo función de importación para no generar error en validación de ID por la última linea en blanco del csv

--- app.js
+++ app.js
@@ -502,7 +502,9 @@ function importarCSV(e) {
 
   const reader = new FileReader();
   reader.onload = evt => {
-    const lineas = dividirLineasCSV(evt.target.result).slice(1);
+    const lineas = dividirLineasCSV(evt.target.result)
+      .slice(1)
+      .filter(l => l.trim() !== "");
     const idsEnCSV = new Set();
     const nuevos = [];
     const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
